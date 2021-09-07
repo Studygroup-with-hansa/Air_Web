7a12c050dc628600f5d4ee6a453cd022a5fa2b63
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-09-07 13:54:45 +0900

    Add: sidebar handleIcon

--- web/src/components/Common/SideBar/SideBar.tsx
+++ web/src/components/Common/SideBar/SideBar.tsx
@@ -2,19 +2,36 @@ import stat from "assets/stat.svg";
 import post from "assets/post.svg";
 import profile from "assets/profile.svg";
 import setting from "assets/setting.svg";
+import { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+
 import "./SideBar.scss";
 
 const SideBar = (): JSX.Element => {
+  const history = useHistory();
   const path = window.location.pathname;
+
+  const handleIcon = useCallback(
+    (url: string) => {
+      history.push(url);
+    },
+    [history]
+  );
 
   return (
     <div className="sideBar">
       <div>
-        <img className={path === "/" ? "isMain" : ""} src={stat} alt="stat" />
+        <img
+          className={path === "/" ? "isMain" : ""}
+          onClick={() => handleIcon("/")}
+          src={stat}
+          alt="stat"
+        />
       </div>
       <div>
         <img
           className={path === "/post" ? "isMain" : ""}
+          onClick={() => handleIcon("/post")}
           src={post}
           alt="post"
         />
@@ -22,6 +39,7 @@ const SideBar = (): JSX.Element => {
       <div>
         <img
           className={path === "/profile" ? "isMain" : ""}
+          onClick={() => handleIcon("/profile")}
           src={profile}
           alt="profile"
         />
@@ -29,6 +47,7 @@ const SideBar = (): JSX.Element => {
       <div>
         <img
           className={path === "/setting" ? "isMain" : ""}
+          onClick={() => handleIcon("/setting")}
           src={setting}
           alt="setting"
         />
