f676af4a2a09f6fa597aee2c70d3898acbc027c0
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-09-14 22:23:03 +0900

    Add: infotext

--- web/src/components/SignUp/SignUp.tsx
+++ web/src/components/SignUp/SignUp.tsx
@@ -1,20 +1,32 @@
+import { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+
 const SignUp = (): JSX.Element => {
+  const history = useHistory();
+  const handleButton = useCallback(() => {
+    history.push("/signin");
+  }, []);
+
   return (
     <>
       <div className="signIn">
         <div className="signIn-content">
           <div>
             <div className="signIn-content-title">회원가입</div>
+            <div>닉네임</div>
+            <input
+              style={{ marginBottom: 24 }}
+              type="text"
+              placeholder="닉네임을 입력해주세요"
+            />
             <div>이메일</div>
             <input type="text" placeholder="이메일을 입력해주세요" />
-            <div>인증코드</div>
-            <input type="text" placeholder="인증 코드를 입력해주세요" />
           </div>
           <div className="signIn-content-button">
             <button>회원가입</button>
             <div>
               회원이신가요?
-              <span> 로그인하기</span>
+              <span onClick={handleButton}> 로그인하기</span>
             </div>
           </div>
         </div>
