c69853cbe33378073fb40998eaeb8f05afa0b5a0
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-10-06 22:21:05 +0900

    Add: image input

--- web/src/components/Profile/Profile.tsx
+++ web/src/components/Profile/Profile.tsx
@@ -1,59 +1,108 @@
 import defaultProfile from "assets/defaultProfile.svg";
-// import { Dropdown } from "react-dropdown-now";
+import { useCallback, useState } from "react";
 
 import "./Profile.scss";
 
+const title = [
+  "칭호1",
+  "칭호2",
+  "칭호3",
+  "칭호4",
+  "칭호5",
+  "칭호6",
+  "칭호7",
+  "칭호8",
+  "칭호9",
+  "칭호10",
+  "칭호11",
+  "칭호12",
+  "칭호13",
+  "칭호14",
+];
+
 const Profile = () => {
-  // const option = [
-  //   "one",
-  //   "two",
-  //   "two",
-  //   "two",
-  //   "two",
-  //   "two",
-  //   "two",
-  //   "two",
-  //   "two",
-  //   "three",
-  // ];
+  const [mainTitle, setMainTitle] = useState<string>("");
+  const [fileUrl, setFileUrl] = useState<string | null>(null);
+
+  const inputImage = useCallback((event) => {
+    const imageFile = event.target.files[0];
+    const imageUrl = URL.createObjectURL(imageFile);
+    setFileUrl(imageUrl);
+  }, []);
+
+  const handleTitle = (data: string) => {
+    setMainTitle(data);
+  };
+
   return (
     <div className="profile">
       <div className="profile-header">내 프로필</div>
       <div className="profile-contents">
         <div className="profile-contents-view">
           <div className="profile-contents-view-img">
-            <img src={defaultProfile} alt="defaultProfile" />
+            <img
+              src={!fileUrl ? defaultProfile : fileUrl}
+              alt="defaultProfile"
+            />
           </div>
-          <div className="profile-contents-view-title">title</div>
+          <div className="profile-contents-view-title">
+            {!mainTitle ? "칭호를 선택해주세요" : mainTitle}
+          </div>
           <div className="profile-contents-view-name">name</div>
         </div>
         <div className="profile-contents-input">
           <div className="profile-contents-input-item">
             <div>
-              닉네임<span className="infotext">8자 이하로 작성해주세요</span>
+              <div className="profile-contents-input-item-title">
+                닉네임<span className="infotext">8자 이내로 작성해주세요</span>
+              </div>
+              <div className="profile-contents-input-item-text">
+                <input type="text" />
+              </div>
             </div>
-            <input type="text" />
+            <div>
+              <div className="profile-contents-input-item-title">
+                프로필 사진
+              </div>
+              <div className="profile-contents-input-item-image">
+                <label
+                  className="profile-contents-input-item-image-button"
+                  htmlFor="input-file"
+                >
+                  <div>이미지 선택</div>
+                </label>
+                <input
+                  type="file"
+                  style={{ display: "none" }}
+                  accept=".jpeg, .jpg, .png"
+                  id="input-file"
+                  onChange={inputImage}
+                />
+              </div>
+            </div>
           </div>
           <div className="profile-contents-input-item">
-            <div>칭호 선택</div>
-            <div className="profile-contents-input-item-title">asdf</div>
-            {/* <Dropdown
-              placeholder="Select an option"
-              className="my-className"
-              options={option}
-              value="one"
-              onChange={(value) => console.log("change!", value)}
-              onSelect={(value) => console.log("selected!", value)} // always fires once a selection happens even if there is no change
-              onClose={(closedBySelection) =>
-                console.log("closedBySelection?:", closedBySelection)
-              }
-              onOpen={() => console.log("open!")}
-            /> */}
+            <div>
+              <div className="profile-contents-input-item-title">칭호</div>
+              <div className="profile-contents-input-item-list">
+                {title.map((data) => {
+                  return (
+                    <div
+                      onClick={() => handleTitle(data)}
+                      className={
+                        mainTitle === data
+                          ? "profile-contents-input-item-list-content mainTitle"
+                          : "profile-contents-input-item-list-content"
+                      }
+                    >
+                      {data}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
-          <div>
-            <div>프로필 사진</div>
-            <div className="profile-contents-input-image"></div>
-          </div>
+          <div className="profile-contents-btn">저장하기</div>
         </div>
       </div>
     </div>
