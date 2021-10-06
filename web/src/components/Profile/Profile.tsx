13464bbce2434f33a3ca317d7c73cef5011aefc6
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-10-06 12:08:55 +0900

    Add: profile input form

--- web/src/components/Profile/Profile.tsx
+++ web/src/components/Profile/Profile.tsx
@@ -1,17 +1,60 @@
-import "./Profile.scss";
 import defaultProfile from "assets/defaultProfile.svg";
+import { Dropdown } from "react-dropdown-now";
+
+import "./Profile.scss";
 
 const Profile = () => {
+  const option = [
+    "one",
+    "two",
+    "two",
+    "two",
+    "two",
+    "two",
+    "two",
+    "two",
+    "two",
+    "three",
+  ];
   return (
     <div className="profile">
       <div className="profile-header">내 프로필</div>
       <div className="profile-contents">
         <div className="profile-contents-view">
-          <div>
+          <div className="profile-contents-view-img">
             <img src={defaultProfile} alt="defaultProfile" />
           </div>
           <div className="profile-contents-view-name">name</div>
           <div className="profile-contents-view-title">title</div>
+        </div>
+        <div className="profile-contents-input">
+          <div className="profile-contents-input-item">
+            <div>
+              닉네임<span className="infotext">8자 이하로 작성해주세요</span>
+            </div>
+            <input type="text" />
+          </div>
+          <div className="profile-contents-input-item">
+            <div>칭호 선택</div>
+            <Dropdown
+              placeholder="Select an option"
+              className="my-className"
+              options={option}
+              value="one"
+              onChange={(value) => console.log("change!", value)}
+              onSelect={(value) => console.log("selected!", value)} // always fires once a selection happens even if there is no change
+              onClose={(closedBySelection) =>
+                console.log("closedBySelection?:", closedBySelection)
+              }
+              onOpen={() => console.log("open!")}
+            />
+          </div>
+          <div className="">
+            <div>프로필 사진</div>
+            <div>
+              <input type="file" name="" id="" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
